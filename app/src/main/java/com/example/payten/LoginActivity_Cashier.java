f715a38c6daa71f9f02b964e7b56a36fa2f1d262
Vasilije <vasilije.kostic2001@gmail.com>
2022-11-29 21:04:27 +0100

    Kasir login menju

---
+++ app/src/main/java/com/example/payten/LoginActivity_Cashier.java
@@ -0,0 +1,14 @@
+package com.example.payten;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity_Cashier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_cashier);
+    }
+}
--- app/src/main/java/com/example/payten/MainMenu.java
+++ app/src/main/java/com/example/payten/MainMenu.java
@@ -2,13 +2,19 @@ package com.example.payten;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
-//[rkdf,;s
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void LoginCashier(View view) {
+        Intent intent = new Intent(MainMenu.this, LoginActivity_Cashier.class);
+        startActivity(intent);
     }
 }
