75a6192b51e216eff518f8a43ac82b02c94ed0ef
Jovan Mihajlović <jovanmihajlovic22@gmail.com>
2022-11-29 21:27:14 +0100

    Rezervacije
    (cherry picked from commit a5f8b09297274fd213a7e6eedfc46d977dc5a768)

--- app/src/main/java/com/example/payten/Product.java
+++ app/src/main/java/com/example/payten/Product.java
@@ -1,16 +1,12 @@
 package com.example.payten;
 
 public class Product {
-    int stock = 0;
-    int ordered = 0;
-    int reserved = 0;
-    int min_value =0;
-    String name;
+    private int count = 0;
+    private int ordered = 0;
+    private int reserved = 0;
+    private String name;
 
-
-
-    public Product(String name, int min_value){
+    public Product(String name) {
         this.name = name;
-        this.min_value=min_value;
     }
 }
