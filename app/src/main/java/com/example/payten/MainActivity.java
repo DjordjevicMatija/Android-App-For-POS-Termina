ca8c4ca2f4bc32df6217de569e76e2c12b49f876
Anjaas85 <119419795+Anjaas85@users.noreply.github.com>
2022-11-29 16:51:23 +0100

    main menu i activity_main.xml

--- app/src/main/java/com/example/payten/MainActivity.java
+++ app/src/main/java/com/example/payten/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.payten;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler mHandler = new Handler();
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                startActivity(intent);
+            }
+            }, 2500);
+
     }
 }
