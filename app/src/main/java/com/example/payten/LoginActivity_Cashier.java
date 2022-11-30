6b8080b3f29940a6777d1f7ca3b9f74505bb1719
Vasilije <vasilije.kostic2001@gmail.com>
2022-11-30 19:58:47 +0100

    Login aktivitiji sa porukama za inkorekt pasvorde

--- app/src/main/java/com/example/payten/LoginActivity_Cashier.java
+++ app/src/main/java/com/example/payten/LoginActivity_Cashier.java
@@ -6,27 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity_Cashier extends AppCompatActivity {
 
     private EditText editTextUsername, editTextPassword;
+    private TextView textViewIncorrect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_cashier);
 
         initialize();
+        textViewIncorrect.setVisibility(View.INVISIBLE);
     }
 
     private void initialize() {
         editTextUsername = (EditText) findViewById(R.id.editTextUsername);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewIncorrect = (TextView) findViewById(R.id.textViewIncorrect2);
     }
 
-    public void LoginManager(View view){
+    public void LoginCashier(View view){
         if(editTextUsername.getText().toString().equals("admin") && editTextPassword.getText().toString().equals("admin")){
             Intent intent = new Intent(LoginActivity_Cashier.this, Seller.class);
             startActivity(intent);
+        }
+        else{
+            textViewIncorrect.setVisibility(View.VISIBLE);
         }
     }
 
--- app/src/main/java/com/example/payten/LoginActivity_Manager.java
+++ app/src/main/java/com/example/payten/LoginActivity_Manager.java
@@ -12,23 +12,29 @@ import android.widget.Toast;
 public class LoginActivity_Manager extends AppCompatActivity {
 
     private EditText editTextUsername, editTextPassword;
+    private TextView textViewIncorrect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_manager);
 
         initialize();
+        textViewIncorrect.setVisibility(View.INVISIBLE);
     }
 
     private void initialize() {
         editTextUsername = (EditText) findViewById(R.id.editTextUsername);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewIncorrect = (TextView) findViewById(R.id.textViewIncorrect);
     }
 
     public void LoginManager(View view){
         if(editTextUsername.getText().toString().equals("admin") && editTextPassword.getText().toString().equals("admin")){
             Intent intent = new Intent(LoginActivity_Manager.this, Manager2_0.class);
             startActivity(intent);
+        }
+        else{
+            textViewIncorrect.setVisibility(View.VISIBLE);
         }
     }
 }
